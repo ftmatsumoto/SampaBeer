bf9d90f42441bb0a955b2dd18d5313327050e966
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2018-06-03 17:45:21 -0300

    (chore) Change filename for Firebase SDK

--- server/firebaseAdminSDK.js
+++ server/firebaseAdminSDK.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./SampaBeer-2e0c90e54c71.json');
+const serviceAccount = require('./SampaBeer-c32b7eec86f5.json');
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://sampabeer2018.firebaseio.com/"
