b9274d72397d8a921d739b4ef507515b1f6341f0
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2018-07-30 23:01:21 -0300

    (fix) Change port number

--- src/actions/wsActions.js
+++ src/actions/wsActions.js
@@ -1,7 +1,8 @@
 import { CLIENT_CONNECT } from './actionTypes';
 
 export function createWSClient(token) {
-  let port = process.env.PORT ? 'https://sampa-beer.herokuapp.com/' + process.env.PORT : 'ws://localhost:8128/';
+  // let port = process.env.PORT ? 'https://sampa-beer.herokuapp.com/' + process.env.PORT : 'ws://localhost:8128/';
+  let port = 'ws://localhost:' + process.env.PORT || '8128/';
   const ws = new WebSocket(port);
 
   return (dispatch) => {
