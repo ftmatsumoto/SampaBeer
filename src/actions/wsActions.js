f49d72042cd709e15f04f362d3bc402c47462c53
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2018-08-04 18:45:50 -0300

    (chore) Add console.log to wsActions

--- src/actions/wsActions.js
+++ src/actions/wsActions.js
@@ -3,8 +3,8 @@ import { CLIENT_CONNECT } from './actionTypes';
 export function createWSClient(token) {
   // let port = process.env.PORT ? 'https://sampa-beer.herokuapp.com/' + process.env.PORT : 'ws://localhost:8128/';
   // console.log(process.env.PORT ? process.env.PORT : '8128/');
-  let port = 'ws://localhost:' + (process.env.PORT ? process.env.PORT : '8128/');
-  console.log(port);
+  let port = 'ws://' + (process.env.PORT ? process.env.PORT : 'localhost:8128/');
+  console.log(process.env.PORT);
   const ws = new WebSocket(port);
 
   return (dispatch) => {
