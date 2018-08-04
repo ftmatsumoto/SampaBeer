a02bae76e5ea4fb3be5f25eb1fa3b6497afec6c2
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2018-08-04 18:57:45 -0300

    (chore) Change url from wsActions.js

--- src/actions/wsActions.js
+++ src/actions/wsActions.js
@@ -3,8 +3,9 @@ import { CLIENT_CONNECT } from './actionTypes';
 export function createWSClient(token) {
   // let port = process.env.PORT ? 'https://sampa-beer.herokuapp.com/' + process.env.PORT : 'ws://localhost:8128/';
   // console.log(process.env.PORT ? process.env.PORT : '8128/');
-  let port = 'ws://' + (process.env.PORT ? process.env.PORT : 'localhost:8128/');
-  console.log(process.env.PORT);
+  // let port = 'ws://' + (process.env.PORT ? process.env.PORT : 'localhost:8128/');
+  let port = 'ws://sampa-beer.herokuapp.com/';
+  // console.log(process.env.PORT);
   const ws = new WebSocket(port);
 
   return (dispatch) => {
